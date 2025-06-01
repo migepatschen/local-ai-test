196c6839b4dff3d91d4bdeb6a4149b3e5737a389
migepatschen <13597235+migepatschen@users.noreply.github.com>
2025-06-01 10:34:39 +0200

    switched to deepseek

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # local-ai-test
 Plays around with Kalosm and ~~Qwen3~~ Tiny LLama
+
+https://masteringbackend.com/posts/building-a-simple-rag-system-application-with-rust
+https://docs.rig.rs/
+https://www.promptingguide.ai/research/rag
--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,19 @@ use kalosm::language::*;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let model = Llama::builder().with_source(LlamaSource::tiny_llama_1_1b_chat()).build().await?;
+    //let model = Llama::builder().with_source(LlamaSource::tiny_llama_1_1b_chat()).build().await?;
+    let model = Llama::builder().with_source(LlamaSource::deepseek_r1_distill_qwen_1_5b()).build().await?;
+    /*let model = Llama::builder().with_source(LlamaSource::new(FileSource::HuggingFace {
+        model_id: String::from("Qwen/Qwen3-0.6B"),
+        revision: String::from("main"),
+        file: String::from("qwen3-0.6b"),
+    })).build().await?;*/
+    
 
     // New code
     let mut chat = model
-        .chat()
-        .with_system_prompt("The assistant will act like a pirate");
+        .chat();
+        //.with_system_prompt("The assistant will act like a pirate");
 
     loop {
         chat(&prompt_input("\n> ")?).to_std_out().await?;
